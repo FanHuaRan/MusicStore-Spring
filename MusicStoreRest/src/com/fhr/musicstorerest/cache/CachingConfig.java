2eeb330d8000b2c8d9a3c3052dfd85a241a0b0db
FanHuaRan <834171100@qq.com>
2017-04-14 20:17:19 +0800

    2017/04/14 fhr

--- MusicStoreRest/src/com/fhr/musicstorerest/cache/CachingConfig.java
+++ MusicStoreRest/src/com/fhr/musicstorerest/cache/CachingConfig.java
@@ -7,28 +7,42 @@ import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import net.sf.ehcache.CacheManager;
 
 /**
- * EhCache配置类 
- * @author ASUS
+ * redis cache配置类 
+ * @author fhr
  *
  */
 
 @Configuration
 @EnableCaching
 public class CachingConfig {
-	//缓存管理器
-	@Bean
-	public EhCacheCacheManager cacheManager(CacheManager cm){
-		return new EhCacheCacheManager(cm);
-	}
-	//工厂bean
-	@Bean
-	public EhCacheManagerFactoryBean ehcache(){
-		EhCacheManagerFactoryBean ehCacheManagerFactoryBean=new EhCacheManagerFactoryBean();
-		ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("com/fhr/musicstorerest/cache/ehcache.xml"));
-		return ehCacheManagerFactoryBean;
-	}
+	@Bean 
+	 public JedisConnectionFactory redisConnectionFactory() { 
+	  JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory(); 
+	  redisConnectionFactory.setHostName("101.200.55.205"); 
+	  redisConnectionFactory.setPort(6379); 
+	  redisConnectionFactory.setPassword("12345678");
+	  return redisConnectionFactory; 
+	 } 
+	 
+	 @Bean 
+	 public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) { 
+	  RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>(); 
+	  redisTemplate.setConnectionFactory(cf); 
+	  return redisTemplate; 
+	 } 
+	 
+	 @Bean 
+	 public RedisCacheManager cacheManager(RedisTemplate redisTemplate) { 
+	  RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate); 
+	  cacheManager.setDefaultExpiration(3000); 
+	  return cacheManager; 
+	 } 
 }
