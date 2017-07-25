4893fc1b404cb033053e13c040a006b903ae3347
FanHuaRan <834171100@qq.com>
2017-07-25 16:11:12 +0800

    2017/07/24 fhr

---
+++ MusicStoreRest/src/com/fhr/musicstorerest/swagger/ApplictionSwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.fhr.musicstorerest.swagger;
+
+import com.mangofactory.swagger.plugin.EnableSwagger;
+/**
+ * 
+ * swagger配置
+ */
+@EnableSwagger
+public class ApplictionSwaggerConfig {
+//	@Bean
+//	public Docket
+}
